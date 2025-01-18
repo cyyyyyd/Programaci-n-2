0f3568d9fb60777ac3d1ec93d98f282d4e9d8ce1
cyyyyyd <Cydchapman@icloud.com>
2025-01-18 19:28:47 -0400

    Add files via upload

---
+++ Programación 2/EjemploHerencia/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Persona
+{
+    public string Nombre { get; set; }
+
+    protected Persona(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
+
+public class Estudiante : Persona
+{
+    public int NumeroUnico { get; set; }
+
+    public Estudiante(string nombre, int numeroUnico) : base(nombre)
+    {
+        NumeroUnico = numeroUnico;
+    }
+}
+
+public class Profesor : Persona
+{
+    public List<Curso> Cursos { get; set; } = new List<Curso>();
+
+    public Profesor(string nombre) : base(nombre) { }
+}
+
+public class Curso
+{
+    public string Nombre { get; set; }
+    public int RecuentoClases { get; set; }
+    public int RecuentoEjercicios { get; set; }
+
+    public Curso(string nombre, int recuentoClases, int recuentoEjercicios)
+    {
+        Nombre = nombre;
+        RecuentoClases = recuentoClases;
+        RecuentoEjercicios = recuentoEjercicios;
+    }
+}
+
+public class Clase
+{
+    public string Identificador { get; set; }
+    public List<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
+    public List<Profesor> Profesores { get; set; } = new List<Profesor>();
+
+    public Clase(string identificador)
+    {
+        Identificador = identificador;
+    }
+}
